4889e753ed0b80e198a80b4f11d78d8a0d1e9ad4
gurdeep kaur <gurdeepkaur@gurdeeps-MacBook-Pro.local>
2018-04-04 16:03:17 +1000

    try to deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,33 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-
-<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
-</head>
-<body>
-
-<div class="autoplay">
-  <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
-<div><h3>1</h3></div>
-<div><h3>2</h3></div>
-<div><h3>3</h3></div>
-<div><h3>4</h3></div>
-<div><h3>5</h3></div>
-<div><h3>6</h3></div>
-</div>
-</div>
-
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="slick/slick.min.js"></script>
-
-<script type="text/javascript">
-    $(elememnt).slick()
-    $('.autoplay').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-    });
-</script>
