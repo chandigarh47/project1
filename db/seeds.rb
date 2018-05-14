3dc15a3c90a396ba7d754b972c1b2940af1b9f55
gurdeep kaur <gurdeepkaur@gurdeeps-MacBook-Pro.local>
2018-05-14 16:31:01 +1000

    seeding agsain

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,11 @@ Hotel.destroy_all
 # h7 = Hotel.create(:name => 'JW Marriot', :room => '400', :room_type => 'Super Deluxe', :price => '360', :amenity => 'Gym,Pool', :image => 'https://image.dhgate.com/0x0/f2/albu/g5/M00/F0/03/rBVaI1mSzyqAGJWyAAIbKlC2qEs022.jpg');
 h1 = Hotel.create :name => 'Royal Inn'
 h2 = Hotel.create :name => 'Sparta Plaza'
-# h3 = Hotel.create :name => 'Hotel Alpha', :room => 320, :room_type => 'Presidential Suit', :price => 350, :amenity => 'Spa,Gym', :image => 'https://img2.10bestmedia.com/Images/Photos/255131/BellagioExterior3_54_990x660_201404251834.jpg';
-# h4 = Hotel.create :name => 'The Quarto', :room => 300, :room_type => 'Queen',  :price => 460, :amenity => 'Spa,Gym,Pool,playing area', :image => 'http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1483032942/delaware-hotel-du-pont-BESTHOTEL1216.jpg?itok=YzAq_AUJ';
-# h5 = Hotel.create :name => 'Turtle on The Beach', :room => 250, :room_type => 'King', :price => 380, :amenity => 'Spa,Gym,Park', :image => 'https://amp.businessinsider.com/images/55f1d8ca9dd7cc11008b97fa-750-563.jpg';
-# h6 = Hotel.create :name => 'Hotel Paradise', :room => 200, :room_type => 'Junoir Suit', :price => 400, :amenity => 'Spa,Gym', :image => 'http://static.asiawebdirect.com/m/phuket/portals/hong-kong-hotels-ws/shared/teasersL/teaser_auto_gen/top10-hotels-pools-hong-kong/teaserMultiLarge/imageHilight/teaser.jpeg.jpg';
-# h7 = Hotel.create :name => 'JW Marriot', :room => 400, :room_type => 'Super Deluxe', :price => 360, :amenity => 'Gym,Pool', :image => 'https://image.dhgate.com/0x0/f2/albu/g5/M00/F0/03/rBVaI1mSzyqAGJWyAAIbKlC2qEs022.jpg';
+h3 = Hotel.create :name => 'Hotel Alpha'
+h4 = Hotel.create :name => 'The Quarto'
+h5 = Hotel.create :name => 'Turtle on The Beach'
+h6 = Hotel.create :name => 'Hotel Paradise'
+h7 = Hotel.create :name => 'JW Marriot'
 
 
 Review.destroy_all
@@ -26,9 +26,12 @@ r2 = Review.create :content => 'bad review'
 r3 = Review.create :content => 'good review'
 
 Booking.destroy_all
-b1 = Booking.create :name => 'bill', :checkin => '10:00am', :checkout => '11:00pm', :room_type => 'deluxe', :price => '300'
-b2 = Booking.create :name => 'jerry', :checkin => '1:00am', :checkout => '09:00pm', :room_type => 'super deluxe', :price => '800'
-b3 = Booking.create :name => 'david', :checkin => '2:00am', :checkout => '04:00pm', :room_type => 'deluxe', :price => '300'
+# b1 = Booking.create :name => 'bill', :checkin => '10:00am', :checkout => '11:00pm', :room_type => 'deluxe', :price => '300'
+# b2 = Booking.create :name => 'jerry', :checkin => '1:00am', :checkout => '09:00pm', :room_type => 'super deluxe', :price => '800'
+# b3 = Booking.create :name => 'david', :checkin => '2:00am', :checkout => '04:00pm', :room_type => 'deluxe', :price => '300'
+b1 = Booking.create :name => 'bill'
+b2 = Booking.create :name => 'jerry'
+b3 = Booking.create :name => 'david'
 
 
 #reviews and hotels
